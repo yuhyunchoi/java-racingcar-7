c8678f974ea9b6d4be98a512ef97c2280b049490
최유현 <dbgus315@naver.com>
2024-10-22 23:16:52 +0900

    fix: 자동차의 이름이 5자 이하만 가능하도록 변경

--- src/main/java/racingcar/model/PlayerInput.java
+++ src/main/java/racingcar/model/PlayerInput.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 
 public class PlayerInput {
     private static final String PLAYER_INPUT_ERROR_MESSAGE = "입력 값이 비어있습니다.";
+    private static final String PLAYER_NUMBER_VALID_ERROR = "자동차의 이름이 5자 이상이거나 비어있습니다.";
+    private static final int PLAYER_CHARACTER_LIMIT = 5;
 
     public List<String> getPlayers(String players){
         if (Objects.isNull(players) || players.trim().isEmpty()) {
@@ -14,7 +16,16 @@ public class PlayerInput {
         }
 
         List<String> playersList = new ArrayList<>(Arrays.asList(players.split((","))));
+        validatorPlayerName(playersList);
         return playersList;
+    }
+
+    public void validatorPlayerName(List<String> playersList) {
+        for(String s : playersList) {
+            if(s.trim().length() > PLAYER_CHARACTER_LIMIT || s.trim().isEmpty()) {
+                throw new IllegalArgumentException(PLAYER_NUMBER_VALID_ERROR);
+            }
+        }
     }
 
 }
